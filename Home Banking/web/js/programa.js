d1ce59270f274b9cc9931cd983e35d38dbe8f754
Usuario <Usuario@A5-M01>
2019-08-16 17:50:58 -0300

    intentando hacer el login con js

---
+++ Home Banking/web/js/programa.js
@@ -0,0 +1,8 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function redireccion(){
+    location.replace("http://localhost:8080/Home_Banking/index.html")
+}
