1f960aa8f4a0f4281e3ed6f937d5d8c04e0791cd
Usuario <Usuario@A5-M01>
2019-08-20 17:49:44 -0300

    JS Login con js

--- Home Banking/web/js/programa.js
+++ Home Banking/web/js/programa.js
@@ -1,8 +1,21 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-function redireccion(){
-    location.replace("http://localhost:8080/Home_Banking/index.html")
+class programa{
+    static login(){
+        console.log("Funciona el login");
+        let usuario =document.querySelector("#usuario").value;
+        let clave   =document.querySelector("#clave").value;
+        
+        let usuarioValido   ="Curtido12@gmai.com";
+        let claveValida     =2019;
+        
+        if (usuarioValido==usuario && claveValida==clave) {
+            console.log("el usuario entro");
+        }else{
+            console.log("no eres usuario del banco");
+        }
+    }
+    static button(){
+        console.log("funciona el button");
+        document.querySelector("#loginBTN").setAttribute("onclick","programa.login();");
+    }
 }
+programa.button();
