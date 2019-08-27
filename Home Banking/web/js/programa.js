9c2161a4feb83eca61a3bdc30899c26152ca080e
Usuario <Usuario@A5-M01>
2019-08-27 17:45:42 -0300

    copia de segurida clases marte 27/08/2019

--- Home Banking/web/js/programa.js
+++ Home Banking/web/js/programa.js
@@ -1,5 +1,28 @@
 class programa {
-    // compraracion de uusuario
+    //                          Base de Datos EN JS.
+    static baseDeDatos() {
+        //base de dato que simula venir del SERVER
+        let cuenta = {
+            nombre : "Marcos Rojas",
+            user : "marcos",
+            pass : "123",
+            saldo : 5000,
+            limite : 2000,
+        };
+        
+        //usamops el localStorage como base de dato 
+        localStorage.setItem("baseDeDatos",JSON.stringify(cuenta));
+        return cuenta;
+    }
+    //            <-------------Fase de estado de cuenta, extraccion, deposito y pago de Cuentas---------> 
+    static consultarCuenta(){
+        console.log("funciona consultarCuentas()");
+        // con este metodo se busca y se meten los datos de la cuenta
+        
+        let misDatos =JSON.parse(localStorage.getItem("baseDeDatos"));
+        document.querySelector()("#bombreUser").innerHTML=misDatos.nombre;
+    }
+    //                          compraracion de usuario
     static login() {
     // Se busca los datos del usuario y se lo guarda en una variable
         console.log("Funciona el login");
@@ -14,24 +37,26 @@ class programa {
             document.querySelector("#LoginPNL").style.display="none";
             document.querySelector("#cuentaPNL").style.display="block";
             document.querySelector("#SMSpass").innerHTML="";
+            programa.consultarCuenta();
         } else {
             console.log("no eres usuario del banco");
             document.querySelector("#SMSpass").innerHTML="Contraceña o Usuario INCORRECTO";
         }
     }
+    //                          metodo Salir Cuenta
     static salir(){
         programa.borrar();
         programa.mostrar();
 //         document.querySelector("#loginPNL").style.display = "block";
     }
-    // funcionamiento de los botones 
+    //                          funcionamiento de los botones 
     static button() {
         console.log("funciona el button");
         document.querySelector("#loginBTN").setAttribute("onclick", "programa.login();"); 
         //document.querySelector("#buttonBRR").setAttribute("onclick","programa.borrar();");
         document.querySelector("#salir").setAttribute("onclick", "programa.salir();");
     }
-    // para ocultar la cuenta Banco
+    // para ocultar o mostrar la cuenta Banco
     static borrar() {
         document.querySelector("#cuentaPNL").style.display = "none";
     }
