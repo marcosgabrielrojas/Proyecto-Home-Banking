304a312c2d306324ff781eb519a1e68cea4a40b7
curti <curti@DESKTOP-0L863RV>
2019-09-02 21:46:03 -0300

    logro CON innerHTML

--- Home Banking/web/js/programa.js
+++ Home Banking/web/js/programa.js
@@ -1,46 +1,46 @@
 class programa {
     //            <-------------Fase de estado de cuenta, extraccion, deposito y pago de Cuentas---------> 
-    
+
     //                          compraracion de usuario
     static login() {
-    // Se busca los datos del usuario y se lo guarda en una variable
+        // Se busca los datos del usuario y se lo guarda en una variable
         console.log("Funciona el login");
         let usuario = document.querySelector("#usuario").value;
         let clave = document.querySelector("#clave").value;
-    // usuario Registrado Trucho
+        // usuario Registrado Trucho
         let usuarioValido = "marcos";
         let claveValida = "123";
-    // Comparacion y validacion de los usuario
+        // Comparacion y validacion de los usuario
         if (usuarioValido === usuario && claveValida === clave) {
             console.log("el usuario entro");
-            document.querySelector("#LoginPNL").style.display="none";
-            document.querySelector("#cuentaPNL").style.display="block";
-            document.querySelector("#SMSpass").innerHTML="";
-            
+            document.querySelector("#LoginPNL").style.display = "none";
+            document.querySelector("#cuentaPNL").style.display = "block";
+            document.querySelector("#SMSpass").innerHTML = "";
+
         } else {
             console.log("no eres usuario del banco");
-            document.querySelector("#SMSpass").innerHTML="Contraceña o Usuario INCORRECTO";
+            document.querySelector("#SMSpass").innerHTML = "Contraceña o Usuario INCORRECTO";
         }
     }
     //                          metodo Salir Cuenta
-    static salir(){
+    static salir() {
         programa.borrar();
         programa.mostrar();
 //         document.querySelector("#loginPNL").style.display = "block";
     }
     //                          funcionamiento de los botones 
     static button() {
-        
-        document.querySelector("#loginBTN").setAttribute("onclick", "programa.login();"); 
+
+        document.querySelector("#loginBTN").setAttribute("onclick", "programa.login();");
         //document.querySelector("#buttonBRR").setAttribute("onclick","programa.borrar();");
         document.querySelector("#salir").setAttribute("onclick", "programa.salir();");
-        document.querySelector("#consultarCuenta").setAttribute("onclick","Cuenta.consultarTpl();");
+        document.querySelector("#consultarCuenta").setAttribute("onclick", "Cuenta.consultarTpl();");
     }
     // para ocultar o mostrar la cuenta Banco
     static borrar() {
         document.querySelector("#cuentaPNL").style.display = "none";
     }
-    static mostrar(){
+    static mostrar() {
         document.querySelector("#LoginPNL").style.display = "block";
     }
 }
@@ -49,23 +49,25 @@ class baseDeDatos {
     static datos() {
         //base de dato que simula venir del SERVER
         let cuenta = {
-            nombre : "MarcosRojas",
-            user : "marcos",
-            pass : "123",
-            saldo : 5000,
-            limite : 2000
+            nombre: "MarcosRojas",
+            user: "marcos",
+            pass: "123",
+            saldo: 5000,
+            limite: 2000
         };
-        
+
         //usamops el localStorage como base de dato 
-        localStorage.setItem("baseDeDatos",JSON.stringify(cuenta));
+        localStorage.setItem("baseDeDatos", JSON.stringify(cuenta));
         return cuenta;
     }
 }
-class Cuenta{
-    static consultarTpl(){
+class Cuenta {
+    static consultarTpl() {
         console.log("funcionaCuenta y consultarTpl()");
-        document.querySelector("#masterTpl").innerHTML =  document.querySelector("#consultarTpl")
-        document.querySelector("")
+        document.querySelector("#masterTpl").innerHTML = document.querySelector("#consultarTpl").innerHTML;
+        // document.querySelector("#consultarTpl").style.display = "block";
+        let  misDatos = JSON.parse(localStorage.getItem("miBaseDeDatos"));
+        document.querySelector("#saldo").innerHtml = "$" + misDatos.saldo;
     }
 }
 
